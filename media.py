66cf957ae68bb5bd9cefe5c546766b9273a504f3
toyjrah <toyjrah@users.noreply.github.com>
2016-08-24 12:25:32 -0400

    Update media.py

--- media.py
+++ media.py
@@ -11,12 +11,14 @@ class Movie():
             movie_storyline,
             poster_image,
             trailer_youtube):
-    ''' Attributes of an object for the class Movie'''
+        ''' Attributes of an object for the class Movie'''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        ''' This function will open the movie trailer on clicking the movie tile on HTML page '''
+        ''' This function will open the movie trailer on clicking the movie"
+            "tile on HTML page '''
         webbrowser.open(self.trailer_youtube_url)
+        
